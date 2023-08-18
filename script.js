1bf16c6dd8d289276c343a6dd114df58bb7dd8e9
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-08-18 20:05:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function decimalToBinary(num) {
   //Write you code here
-  let x;
-	for(let i=0;i<num;i++){
-		if(num%2===0){
-		x = x + "0";
-			num = num/2;
-		}else{
-			x = x + "1";	
-			num = num/2;
-		}
-	}
-	console.log(x);
+ return num.toString(2);
 }
 
 window.decimalToBinary = decimalToBinary;
